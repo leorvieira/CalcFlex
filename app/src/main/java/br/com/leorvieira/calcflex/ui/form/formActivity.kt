81aae715727d7a386ff4140454aea5910b93379e
Logon Aluno <logonaluno@local.com>
2019-12-14 11:15:52 -0300

    Initial commit

---
+++ app/src/main/java/br/com/leorvieira/calcflex/ui/form/formActivity.kt
@@ -0,0 +1,13 @@
+package br.com.leorvieira.calcflex.ui.form
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.leorvieira.calcflex.R
+
+class formActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_form)
+    }
+}
---
+++ app/src/main/java/br/com/leorvieira/calcflex/ui/login/loginActivity.kt
@@ -0,0 +1,13 @@
+package br.com.leorvieira.calcflex.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.leorvieira.calcflex.R
+
+class loginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
